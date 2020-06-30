340e0708c2b346412127dac9579c407cb907320b
suryakasal <60033983+suryakasal@users.noreply.github.com>
2020-06-30 19:09:12 +0530

    upload

---
+++ about.php
@@ -0,0 +1,2 @@
+<h2>About Us</h2>
+<h4>hey people we are excited to tell you that we are here to notify about the interests you love to here..as fast as possible..</h4>
---
+++ contact.php
@@ -0,0 +1,26 @@
+
+<form method="post">
+	<div class="row">
+		<div class="col-md-8">
+		<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	
+	
+	
+	
+	
+		
+		</div>
+	
+		<h2>Contact us</h2>
+		
+		Name: Surya & Vishal<br><br>
+		Mobile:7795303538/8792697704<br><br>
+		Email:<a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZcRThjTkbqzChLgWkhVWrJLQpNXpPDlXhFlmRfdMWDZNCnBhWQmCHXLvSRCNNdtMtsbRg">kasalasurya16@gmail.com</a><br><br>
+		</div>
+	</div>
+	
+	
+</form>	
